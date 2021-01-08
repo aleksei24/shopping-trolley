42fa0dfedbbb449f15bd3160a3a102c393598239
aleksei24 <lexa24032@gmail.com>
2021-01-08 16:27:37 +0300

    now we are getting the array from json file

--- js/script.js
+++ js/script.js
@@ -22,9 +22,16 @@ class Products {
         try {
             let result = await fetch(path);
             let data = await result.json();
-            return data;
-        } catch (error) {
-            console.log(error);
+            let products = data.items;
+            products = products.map((item) => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image };
+            });
+            return products;
+        } catch (err) {
+            console.log(err);
         }
     }
 }
