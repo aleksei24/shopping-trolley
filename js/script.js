89d64e702cbc9a4a425a19bcc650931ecc58cd86
aleksei24 <lexa24032@gmail.com>
2021-01-12 22:21:16 +0300

    scrit for contentful has been added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: '0im7ih6gsp87',
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: 'QcjPpITwdiDAjVXdKSR543T5XYUu66lbiM4BBKFbsxE',
+});
+
 // variables
 const trolleyBtn = document.querySelector('.trolley-btn');
 const closeTrolleyBtn = document.querySelector('.close-trolley');
@@ -23,9 +30,14 @@ class Products {
     // method
     async getProducts() {
         try {
-            let result = await fetch(path);
-            let data = await result.json();
-            let products = data.items;
+            let contentful = await client.getEntries({
+                content_type: 'testShop',
+            });
+
+            /*let result = await fetch(path);
+            let data = await result.json();*/
+
+            let products = contentful.items;
             products = products.map((item) => {
                 const { title, price } = item.fields;
                 const { id } = item.sys;
