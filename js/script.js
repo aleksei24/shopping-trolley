f6f791db44152d0488019c8f404810c4558df516
aleksei24 <lexa24032@gmail.com>
2021-04-18 17:55:23 +0300

    variables are declared

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// variables
+const trolleyBtn = document.querySelector('.trolley-btn');
+const closeTrolleyBtn = document.querySelector('.close-trolley');
+const clearTrolley = document.querySelector('.clear-trolley');
+const trolleyDOM = document.querySelector('.trolley');
+const trolleyOverlay = document.querySelector('.trolley-overlay');
+const trolleyItems = document.querySelector('.trolley-items');
+const trolleyTotal = document.querySelector('.trolley-total');
+const trolleyContent = document.querySelector('.trolley-content');
+const productsDOM = document.querySelector('.products-center');
+
+// trolley
+let trolley = [];
